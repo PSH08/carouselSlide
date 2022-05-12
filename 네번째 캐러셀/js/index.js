ae4fe21040b96b4b9796f1a4f040f940e6d8b5be
lolu1023 <soowkw@naver.com>
2022-05-12 23:40:55 +0900

    4번째 다중 슬라이드 완성

--- 네번째 캐러셀/js/index.js
+++ 네번째 캐러셀/js/index.js
@@ -2,35 +2,47 @@ const slides = document.querySelector('.slides');
 const slide = document.querySelectorAll('.slides li');
 const prevBtn = document.querySelector('.prev');
 const nextBtn = document.querySelector('.next');
-let currentIdx = 0
 let slideLen = slide.length
-let count = 1
+let counter = 0
 
 let slideWidth = 300 
 let slideMargin =  30
 
 slides.style.width = (slideWidth + slideMargin) * slideLen - slideMargin + 'px';
 
-function moveSlide(num) {
-  slides.style.left = -num * 330 + 'px'
-  // slides.style.left = -count * (slideWidth + slideMargin) + 'px'
-  currentIdx = num;
-}
-nextBtn.addEventListener('click', function() {
-  if(currentIdx < slideLen-3) {
-    moveSlide(currentIdx + 1)
-  } else {
-    moveSlide(0)
-  }
-});
-// nextBtn.addEventListener('click', next)
-
-// function next() {
-//   if(slideLen === 8) {
-//     currentIdx++
-//   }else {
-//     currentIdx = 0;
-//   }
-//   moveSlide()
+// function moveSlide(num) {
+//   slides.style.left = -num * (slideWidth+slideMargin) + 'px'
+//   // slides.style.left = -count * (slideWidth + slideMargin) + 'px'
+//   currentIdx = num;
 // }
-// console.log(slideLen)
+// nextBtn.addEventListener('click', function() {
+//   if(currentIdx < slideLen-3) {
+//     moveSlide(currentIdx + 1)
+//   } else {
+//     moveSlide(0)
+//   }
+// });
+nextBtn.addEventListener('click', nextClick)
+prevBtn.addEventListener('click',prevClick)
+
+function moveSlide() {
+  slides.style.left = -counter * (slideWidth + slideMargin) + 'px'
+}
+function nextClick() {
+  if(counter < slideLen-3) {
+    counter++
+  }else {
+    counter = 1
+  }
+  console.log(counter)
+  moveSlide()
+}
+function prevClick() {
+  if(counter > 0) {
+    counter--
+  }else {
+    counter = slideLen-3
+  }
+  console.log(counter)
+  moveSlide()
+}
